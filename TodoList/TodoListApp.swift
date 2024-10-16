936fbad1c75acf760ad1863aa4aa7c7e9c6135a4
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-10-16 11:34:12 -0500

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Alejandro La Rosa on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
