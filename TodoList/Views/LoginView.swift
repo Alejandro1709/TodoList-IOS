cec87c096675f8614858e5768d4e25bd177e0d7e
Alejandro LR <alejandro.lr.developer@hotmail.com>
2024-10-16 14:45:10 -0500

    Add: Login view

--- TodoList/Views/LoginView.swift
+++ TodoList/Views/LoginView.swift
@@ -5,4 +5,52 @@
 //  Created by Alejandro La Rosa on 16/10/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                    
+                    Button {
+                        // Attempt login
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.blue)
+                            Text("Log In")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                    .padding()
+                }
+                
+                // Create account
+                VStack {
+                    Text("New Around Here?")
+                    NavigationLink("Create An Account", destination: RegisterView())
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
